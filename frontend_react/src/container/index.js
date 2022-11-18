d43dabb3c062e5d213bec1b79e8045d4a04c0459
Kennedy Melo <kennedymelolp@gmail.com>
2022-11-17 21:16:05 -0300

    Draft: v1.0 Navbar

---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,15 @@
+import About from './About/About';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Skills from './Skills/Skills';
+import Work from './Work/Work';
+import Testimonials from './Testimonials/Testimonials';
+
+export {
+    About,
+    Footer,
+    Header,
+    Skills,
+    Work,
+    Testimonials,
+};

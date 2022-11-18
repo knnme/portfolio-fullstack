1c8790af2e5958a3785817e34a9c131ae84fed20
Kennedy Melo <kennedymelolp@gmail.com>
2022-11-18 08:02:37 -0300

    Draft: v1.2 Backend integration

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,19 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-  { title: 'Web Developement', description: 'I am a good web developer.', imgUrl: images.about01},
-  { title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
-  { title: 'Ui/Ux', description: 'I am a good web developer.', imgUrl: images.about03},
-  { title: 'Web Animation', description: 'I am a good web developer.', imgUrl: images.about04},
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+     .then((data) => setAbouts(data))
+  }, []);
+
+
   return (
     <>
       <h2 className='head-text'>
@@ -29,7 +33,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20}}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10}}>{about.description}</p>
           </motion.div>
